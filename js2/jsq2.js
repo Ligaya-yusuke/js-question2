6796aae94a23d5449847bccd396c89f557243f07
Ligaya-yusuke <e98069@gmail.com>
2020-12-29 04:45:51 +0900

    課題②修正1

--- js2/jsq2.js
+++ js2/jsq2.js
@@ -1,28 +1,64 @@
 'use strict'
 
-document.getElementById('button').addEventListener('click', ()=>{
-  
-  const fizzNum = document.querySelector('input[name="fizzNum"]');
-  const buzzNum = document.querySelector('input[name="buzzNum"]');
-  
-  for (let i = 1; i < 100; i++){
-    if ((fizzNum.value === '') || (buzzNum.value === '') || (fizzNum.value.match(/[a-z]\.d/g)) || (buzzNum.value.match(/[a-z]\.d/g))) {
-      const errorMessage = document.createElement('li');
-      errorMessage.textContent = '整数値を入力してください';
-      document.querySelector('ul').appendChild(errorMessage);
+document.getElementById('button').addEventListener('click', ()=> {
+  const fizz = document.querySelector('input[name="fizz"]').value;
+  const fizzNum = Number.parseFloat(fizz, 10);
+  const buzz = document.querySelector('input[name="buzz"]').value;
+  const buzzNum = Number.parseFloat(buzz, 10);
+  const error = () => {
+    const errorMessage = document.createElement('li');
+    errorMessage.textContent = '整数値を入力してください';
+    document.querySelector('ul').appendChild(errorMessage);
+    document.getElementById('button').addEventListener('click', () => {
+      document.querySelector('ul').remove(errorMessage);
+    })
+  }
+  for (let i = 1; i < 100; i++) {
+    if(fizz === '') {
+      error();
       break;
-    } else if(i % fizzNum.value === 0 && i % buzzNum.value === 0) {
+    }
+    if(buzz === '') {
+      error();
+      break;
+    } 
+    if(fizz.match(/\d\.d/)) {
+      error();
+      break;
+    }
+    if(buzz.match(/\d\.d/)) {
+      error();
+      break;
+    }
+    if(fizz.match(/[A-Za-z]/)) {
+      error();
+      break;
+    } 
+    if(buzz.match(/[A-Za-z]/)) { 
+      error();
+      break;
+    } 
+    if((i % fizzNum === 0) && (i % buzzNum === 0)) {
       const fizzBuzzList = document.createElement('li');
-      fizzBuzzList.textContent = 'FizzBuzz ' + i;
+      fizzBuzzList.textContent = `FizzBuzz ${i}`;
       document.querySelector('ul').appendChild(fizzBuzzList)
-    } else if (i % buzzNum.value === 0) {
+      document.getElementById('button').addEventListener('click', () => {
+        document.querySelector('ul').removeChild(fizzBuzzList);
+      })
+    } else if (i % buzzNum === 0) {
       const buzzList = document.createElement('li');
-      buzzList.textContent= 'Buzz ' + i;
-      document.querySelector('ul').appendChild(buzzList); 
-    } else if (i % fizzNum.value === 0) {
+      buzzList.textContent = `Buzz ${i}`;
+      document.querySelector('ul').appendChild(buzzList);
+      document.getElementById('button').addEventListener('click', () => {
+        document.querySelector('ul').removeChild(buzzList);
+      })
+    } else if (i % fizzNum === 0) {
       const fizzList = document.createElement('li');
-      fizzList.textContent = 'Fizz ' + i;
-      document.querySelector('ul').appendChild(fizzList)
-    };
+      fizzList.textContent = `Fizz ${i}`;
+      document.querySelector('ul').appendChild(fizzList);
+      document.getElementById('button').addEventListener('click', () => {
+        document.querySelector('ul').removeChild(fizzList);
+      })
+    }
   };
 });
